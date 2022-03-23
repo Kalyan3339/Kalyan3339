0843f51889d01952102d7db94b12809c638c6c4e
KalyanReddy <haripriyakalyan02@gmail.com>
2022-03-23 23:12:32 +0530

    test files

---
+++ test.java
@@ -0,0 +1,4 @@
+hellooo 
+thank you the part of github 
+please learn qukliy
+
---
+++ test.sh
@@ -0,0 +1,6 @@
+hii friend
+how are u
+iam from tirupathi
+this is github demo class
+and this is very importent 
+please follow the commands 
