b14cda6cf00b04bebf55fd5c6d371d89fab2d23c
KalyanReddy <haripriyakalyan02@gmail.com>
2022-03-23 23:19:12 +0530

    updated

--- UsersBean.java
+++
@@ -1,4 +0,0 @@
-Helloo Friends
-Iam very good at Dockers
-Iam from TiruPTHI
-GITHUB is very importent topic
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-hello friend 
-this is gitHub demo class
-please learn the GitHub commands
-
